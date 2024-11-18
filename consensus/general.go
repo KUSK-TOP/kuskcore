f1bd6bd919a15bf44bbd6197547c226cd759755b
DrewKusk <k@kusk.top>
2024-11-18 10:13:21 +0800

    fix: a bug fix

--- consensus/general.go
+++ consensus/general.go
@@ -125,7 +125,7 @@ var MainNetParams = Params{
 	CasperConfig: CasperConfig{
 		BlockTimeInterval:   6000,
 		MaxTimeOffsetMs:     3000,
-		BlocksOfEpoch:       100,
+		BlocksOfEpoch:       20,
 		MinValidatorVoteNum: 3e12,
 		VotePendingBlockNums: []VotePendingBlockNum{
 			{BeginBlock: 0, EndBlock: math.MaxUint64, Num: defaultVotePendingNum},
